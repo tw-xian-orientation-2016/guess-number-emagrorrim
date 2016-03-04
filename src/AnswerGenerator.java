bad64a728e8133ef0d191d84235155b79abba352
emagrorrom <373407641@qq.com>
2016-03-04 14:36:02 +0800

    finish generator method in AnswerGenerator

--- src/AnswerGenerator.java
+++ src/AnswerGenerator.java
@@ -1,8 +1,24 @@
-/**
- * Created by apple on 16/3/4.
- */
+import java.util.Random;
+
 public class AnswerGenerator {
-//    public String generate() {
-//
-//    }
+    private Random random;
+
+    public AnswerGenerator(Random random) {
+        this.random = random;
+    }
+
+    public String generate() {
+
+        String number = "";
+
+        while (number.length() < 4) {
+            String digit = String.valueOf(random.nextInt(10));
+
+            if (!number.contains(digit)) {
+                number += digit;
+            }
+        }
+
+        return number;
+    }
 }
